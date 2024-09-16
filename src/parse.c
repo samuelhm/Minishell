34beae69ffc0fcbf59a089a9028b819689e351d4
Lin Yao <linyao@LindeMacBook-Pro.local>
2024-09-17 00:59:37 +0200

    parse 1

--- src/parse.c
+++ src/parse.c
@@ -38,6 +38,63 @@ bool	check_quote(char *s)
 	return (true);
 }
 
+bool    check_quote(char *s)
+{
+	bool	single_quote_close = true;
+	bool	double_quote_close = true;
+	int	flag = 0;
+
+	while (*s)
+	{
+		if (*s == '\'')
+		{
+			if (flag == DOUBLE_QUOTE && double_quote_close == false)
+				break ;
+			else if (flag == DOUBLE_QUOTE && double_quote_close == true)
+			{
+				flag = SINGLE_QUOTE;
+				if (single_quote_close == false)
+					single_quote_close = true;
+				else if (single_quote_close == true)
+					single_quote_close = false;
+			}
+			else if (flag == SINGLE_QUOTE && single_quote_close == false)
+				single_quote_close = true;
+			else if (flag == SINGLE_QUOTE && single_quote_close == true)
+				single_quote_close = false;
+			else if (flag == 0)
+			{
+				flag = SINGLE_QUOTE;
+				single_quote_close = false;
+			}
+		}
+		else if (*s == '\"')
+		{
+			if (flag == SINGLE_QUOTE && single_quote_close == false)
+				break ;
+			else if (flag == SINGLE_QUOTE && single_quote_close == true)
+			{
+				flag = DOUBLE_QUOTE;
+				if (double_quote_close == false)
+					double_quote_close = true;
+				else if (double_quote_close == true)
+					double_quote_close = false;
+			}
+			else if (flag == DOUBLE_QUOTE && double_quote_close == false)
+				double_quote_close = true;
+			else if (flag == DOUBLE_QUOTE && double_quote_close == true)
+				double_quote_close = false;
+			else if (flag == 0)
+			{
+				flag = DOUBLE_QUOTE;
+				double_quote_close = false;
+			}
+		}
+		s++;
+	}
+	return (single_quote_close && double_quote_close);
+}
+
 void	split_into_arrays(char ***new, char *input)
 {
 	char	*str;
