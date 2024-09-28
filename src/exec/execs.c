cb88d2a204599141486e6f83712ce839c6b2e354
Samuel Hurtado Marin <shurtado@student.42barcelona.com>
2024-09-28 12:31:42 +0200

    Added array protection

--- src/exec/execs.c
+++ src/exec/execs.c
@@ -6,7 +6,7 @@
 /*   By: shurtado <shurtado@student.42barcelona.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 20:04:36 by shurtado          #+#    #+#             */
-/*   Updated: 2024/09/28 12:27:22 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/09/28 12:31:25 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 int	process_line(t_ms *ms)
 {
+	char	*path;
+
+	path = ft_strjoin("/bin/", ms->av[0]);
+	if (!path)
+		return (1);
 	if (has_any_redirect(ms->av))
 		do_redirection(ms->av);
 	if (is_builtin(ms->av))
@@ -22,7 +27,8 @@ int	process_line(t_ms *ms)
 	}
 	else
 	{
-		execute_command(ft_strjoin("/bin/",ms->av[0]), ms->av, get_env_arr(ms));
+		execute_command(ft_strjoin(path, ms->av[0]), ms->av, get_env_arr(ms));
+		free(path);
 	}
 	return (0);
 }
