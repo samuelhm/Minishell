d33c79e57121bb20de1ab5ab8c93badcf415ca03
Samuel Hurtado <lnx.wifi@gmail.com>
2024-10-06 13:48:30 +0200

    fixed blt_unset

--- src/blt/unset.c
+++ src/blt/unset.c
@@ -6,7 +6,7 @@
 /*   By: shurtado <shurtado@student.42barcelona.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 13:09:57 by shurtado          #+#    #+#             */
-/*   Updated: 2024/09/21 17:03:58 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/10/06 13:46:57 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	blt_unset(char **av, t_hash *env)
 	int	i;
 
 	i = 1;
-	if (av[1])
+	if (!av[1])
 	{
 		ft_printf("unset: not enough arguments\n");
 		return (1);
